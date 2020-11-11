8f50bb761a2d4088c824b2303f699380e197abe7
msharma55578 <m.sharma55578@gmail.com>
2020-11-12 00:27:29 +0530

    pre final commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Todo from "./Todo"
+import JokeGenerator from './components/JokeGenerator';
+import {Switch,Route,BrowserRouter as Router} from "react-router-dom";
+import Form from "./components/Form.js";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <Multi/> */}
+      <Router>
+          <Switch>
+            <Route exact path="/" exact component={JokeGenerator}/>
+            <Route exact path="/path2" component={Todo}/>
+          </Switch>
+      </Router>
     </div>
   );
 }
